42a42e20747941dbf97ab411e791618a61491dd7
Thomas Seife <92551215+Teseife@users.noreply.github.com>
2025-05-20 18:40:13 -0400

    Turned aurora text back to slate. Also changes the theme-provider.tsx so that it will only alter the color scheme of a page based on toggle button only.

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,11 +1,32 @@
-"use client"
+// "use client"
+//
+// import * as React from "react"
+// import { ThemeProvider as NextThemesProvider } from "next-themes"
+//
+// export function ThemeProvider({
+//                                   children,
+//                                   ...props
+//                               }: React.ComponentProps<typeof NextThemesProvider>) {
+//     return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+// }
+// components/ui/theme-provider.tsx
+"use client";
 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function ThemeProvider({
                                   children,
                                   ...props
                               }: React.ComponentProps<typeof NextThemesProvider>) {
-    return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+    return (
+        <NextThemesProvider
+            {...props}
+            attribute="class"       // use class strategy
+            defaultTheme="light"    // or "dark", whichever you prefer as initial
+            enableSystem={false}    // ← disable system-based theming
+        >
+            {children}
+        </NextThemesProvider>
+    );
 }
